9211099d1749974565999bb7e0718c8689e2c688
carloscordova.dev <cordovamanuel22@icloud.com>
2024-08-08 12:52:30 -0700

    perf(endpoints): :construction: commented ADMIN verification code to add items from postman without validation

--- endpoints/items.js
+++ endpoints/items.js
@@ -5,9 +5,9 @@ const { secret } = require('../config.js')
 router.post('/', validateToken(secret), (req, res) => {
   const { role } = req.user // Obtener el rol del usuario desde el token decodificado
 
-  if (role !== 'ADMIN') {
-    return res.status(403).send({ message: 'Forbidden. Only ADMIN can create items.' })
-  }
+  // if (role !== 'ADMIN') {
+  //   return res.status(403).send({ message: 'Forbidden. Only ADMIN can create items.' })
+  // }
 
   const item = req.body
   router.db.get('items').push(item).write()
